c18e8df757d50be36472d36f3afa060141f16ea7
Eashan Joshi <eashanj4@gmail.com>
2025-06-23 11:26:45 -0500

    New line

--- src/config/index.ts
+++ src/config/index.ts
@@ -28,7 +28,7 @@ export const SITE_CONTENT: SiteContent = {
     name: "Eashan M. Joshi",
     specialty: "Software Engineer",
     summary:
-      "AI-driven Software Engineer blending deep tech expertise with a passion for fitness and mental resilience. I build intelligent, high-performance apps, and if we’re building together, don’t be surprised  if you start optimizing your health like your code, and tune into the Huberman Lab podcast.",
+      "Software Engineer with deep expertise in AI and systems design, driven by a passion for fitness and mental resilience. I build intelligent, high-performance apps, and if we’re building together, don’t be surprised  if you start optimizing your health like your code, and tune into the Huberman Lab podcast.",
     email: "example@email.com",
   },
   experience: [
